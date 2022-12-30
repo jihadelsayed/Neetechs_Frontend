68bd77aca874567d5df08dfcf789c2f1f45f0dea
jihad <david.pales1@gmail.com>
2022-12-30 22:38:33 +0100

    sdfs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,15 @@
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NotAuthGuard } from './authentication/services/not-auth.guard';
-import { SignInComponent } from './authentication/sign-in/sign-in.component';
-import { SignUpComponent } from './authentication/sign-up/sign-up.component';
-import { AuthGuard } from './authentication/services/auth.guard';
+
 import { NotFoundComponent } from './not-found/not-found.component';
+import { NotAuthGuard } from './authorization/services/not-auth.guard';
 
 const routes: Routes = [
   // authentication component
   { path:'', pathMatch:'full', redirectTo:'home'},
-
   //{ path:'', pathMatch:'full', redirectTo:'home' ,component: AppComponent,canActivate:[AuthGuard] },
   { path:'home' ,component: AppComponent,canActivate:[NotAuthGuard] },
-
-  { path:'signUp',component: SignUpComponent,canActivate:[NotAuthGuard] },
-  { path:'signIn',component: SignInComponent,canActivate:[NotAuthGuard] },
   { path:'**',component: NotFoundComponent,canActivate:[NotAuthGuard] },
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,23 @@
-import { NotAuthGuard } from './authentication/services/not-auth.guard';
-import { SignUpComponent } from './authentication/sign-up/sign-up.component';
-import { SignInComponent } from './authentication/sign-in/sign-in.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthenticationComponent } from './authentication/authentication.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { UserService } from './authentication/services/user.service';
 import { FormsModule } from '@angular/forms';
-import { AuthGuard } from './authentication/services/auth.guard';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { JwtInterceptor } from './authentication/Jwt-interceptor.interceptor';
 import { AuthorizationComponent } from './authorization/authorization.component';
+import { JwtInterceptor } from './authorization/Jwt-interceptor.interceptor';
+import { NotAuthGuard } from './authorization/services/not-auth.guard';
+import { AuthGuard } from './authorization/services/auth.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthenticationComponent,
-    SignInComponent,
-    SignUpComponent,
+    AuthorizationComponent,
     NotFoundComponent,
     AuthorizationComponent
   ],
@@ -34,7 +28,7 @@ import { AuthorizationComponent } from './authorization/authorization.component'
     FormsModule,
     HttpClientModule
   ],
-  providers: [{provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },UserService,NotAuthGuard,AuthGuard],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },NotAuthGuard,AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
