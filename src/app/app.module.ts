a7a31bb75efeeb7381f4328f13ac058b27f036e7
Jihad El Sayed <david.pales1@gmail.com>
2023-05-11 03:51:39 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { HomeComponent } from './home/home.component';
+import { MaterialModule } from './material';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NotFoundComponent } from './not-found/not-found.component';
+
 import { AuthorizationComponent } from './authorization/authorization.component';
 import { JwtInterceptor } from './authorization/Jwt-interceptor.interceptor';
 import { NotAuthGuard } from './authorization/services/not-auth.guard';
 import { AuthGuard } from './authorization/services/auth.guard';
-import { HomeComponent } from './home/home.component';
-import { MaterialModule } from './material';
 
+
+
+
+// header
 import { LoginMenuComponent } from './header/login-menu/login-menu.component';
 import { UserMenuComponent } from './header/user-menu/user-menu.component';
 import { UserNotificationsMenuComponent } from './header/user-notifications-menu/user-notifications-menu.component';
