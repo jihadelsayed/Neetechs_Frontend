fec4dfbdbb6408a73163fd4bcd453997ee281507
jihad <david.pales1@gmail.com>
2024-06-12 20:22:09 +0200

    localize

---
+++ src/environments/menu.ts
@@ -0,0 +1,36 @@
+export const menu = [
+  {
+    id: 1,
+    title: "creed",
+    routerLink: "creed",
+  },
+  {
+    id: 2,
+    title: "pillars",
+    routerLink: "pillars",
+  },
+  {
+    id: 3,
+    title: "provisions",
+    routerLink: "provisions",
+  },
+  {
+    id: 4,
+    title: "jurisprudence",
+    routerLink: "jurisprudence",
+  },
+  {
+    id: 4,
+    title: "rhetoric",
+    routerLink: "rhetoric",
+  },
+  {
+    id: 6,
+    title: "muslims",
+    routerLink: "muslims",
+  }
+
+]
+// الله الملائكة الجمادات النباتات الجن البشر
+// الجبار جبار مجبار جبريل
+// جبريل معجون من جبروت الله
