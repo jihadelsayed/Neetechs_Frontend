97d98374e777d172351d72b588f39e29159ea2fa
jihadelsayed <david.pales1@gmail.com>
2024-06-23 17:49:07 +0200

    dasf

--- src/app/components/blog-news/blog-news-categories/blog-news-categories.component.ts
+++ src/app/components/blog-news/blog-news-categories/blog-news-categories.component.ts
@@ -1,14 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { BlogNewsCategoriesService } from './blog-news-categories.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-blog-news-categories',
   standalone: true,
   imports: [CommonModule,HttpClientModule ],
   templateUrl: './blog-news-categories.component.html',
-  styleUrl: './blog-news-categories.component.scss'
+  styleUrl: './blog-news-categories.component.scss',
+  providers: [BlogNewsCategoriesService]
 })
 export class BlogNewsCategoriesComponent {
+  category: any;
 
+  constructor(private route: ActivatedRoute, private BlogNewsCategoriesService: BlogNewsCategoriesService) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const categoriesId = params.get('categoriesId');
+      if (categoriesId) {
+        this.fetchData(categoriesId);
+      }
+    });
+  }
+
+  private fetchData(categoriesId: string): void {
+    this.BlogNewsCategoriesService.getCategoryData(categoriesId).subscribe(data => {
+      this.category = data;
+    });
+  }
 }
