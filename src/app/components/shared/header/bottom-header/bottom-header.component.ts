96a66ac0dfc25a6658436205c787c9ce1461252a
jihadelsayed <david.pales1@gmail.com>
2024-07-21 00:53:33 +0200

    cryp

--- src/app/components/shared/header/bottom-header/bottom-header.component.ts
+++ src/app/components/shared/header/bottom-header/bottom-header.component.ts
@@ -1,5 +1,5 @@
 import { NgClass, CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { menu_data } from '../menu-data';
 
@@ -12,7 +12,16 @@ import { menu_data } from '../menu-data';
 })
 export class BottomHeaderComponent {
   public menu_data:any[] = menu_data
+  @Output() menuToggle: EventEmitter<void> = new EventEmitter<void>();
+  toggleMenu() {
+    this.menuToggle.emit();
+  }
 
+  navigateToTop() {
+    window.scrollTo(0, 0);
+    this.toggleMenu();
+
+  }
   constructor() {}
 
 
