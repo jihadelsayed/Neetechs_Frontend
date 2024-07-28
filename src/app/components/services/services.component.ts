2b8662bb25e8327cac408258507f89f059a59e74
jihadelsayed <david.pales1@gmail.com>
2024-07-28 04:33:45 +0200

    services

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { ServicesHeroComponent } from './services-hero/services-hero.component';
+import { CommonModule } from '@angular/common';
+import { ServicesService } from './services.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [ServicesHeroComponent],
+  providers: [ServicesService],
+
+  imports: [ServicesHeroComponent,CommonModule],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
 export class ServicesComponent {
+  services:any;
+  constructor(private servicesService: ServicesService,private router: Router) { }
 
+  ngOnInit(): void {
+    this.servicesService.getServicesData().subscribe((data:any) => {
+      this.services = data.services;
+    });
+  }
+  navigateTo(url: string): void {
+    this.router.navigate([url]);
+  }
 }
