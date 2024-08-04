6c60150eebf8990b6a3763811d2b3ce3e1f49e17
jihadelsayed <david.pales1@gmail.com>
2024-08-04 18:22:05 +0200

    emoje add

--- src/app/components/tools/tools-categories/tool/tool.component.ts
+++ src/app/components/tools/tools-categories/tool/tool.component.ts
@@ -3,55 +3,76 @@ import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToolService } from './tool.service';
 import { CommonModule } from '@angular/common';
+import { ContactBannerComponent } from "../../../shared/contact-banner/contact-banner.component";
 
 @Component({
   selector: 'app-tool',
   standalone: true,
-  imports: [FormsModule,CommonModule ],
+  imports: [FormsModule, CommonModule, ContactBannerComponent],
   providers: [ToolService],
   templateUrl: './tool.component.html',
-  styleUrl: './tool.component.scss'
+  styleUrls: ['./tool.component.scss'],
 })
 export class ToolComponent {
   tool: any;
   categoriesId: string | null = null;
   toolId: string | null = null;
-  constructor(private route: ActivatedRoute,private router: Router, private toolService: ToolService) { }
+  inputText: string = '';
+  translatedText: string = '';
+  translationMode: string = 'add-emojis-to-text'; // Default mode
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private toolService: ToolService
+  ) {}
+
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       this.categoriesId = params.get('categoriesId');
-      this.toolId = "none"//params.get('toolId');
+      this.toolId = 'none'; // params.get('toolId');
 
       if (this.categoriesId && this.toolId) {
-        this.fetchData(this.categoriesId,this.toolId);
-      }else if(this.categoriesId){
-        this.router.navigate(['/tools/'+this.categoriesId]);
-      }else{
+        this.fetchData(this.categoriesId, this.toolId);
+      } else if (this.categoriesId) {
+        this.router.navigate(['/tools/' + this.categoriesId]);
+      } else {
         this.router.navigate(['/tools/']);
       }
     });
   }
 
   private fetchData(categoriesId: string, toolId: string): void {
-    this.toolService.getCategoryData(categoriesId, toolId).subscribe((data: any) => {
-      this.tool = data.data;
-    });
+    this.toolService
+      .getCategoryData(categoriesId, toolId)
+      .subscribe((data: any) => {
+        this.tool = data.data;
+      });
   }
+
   navigateTo(url: string): void {
     this.router.navigate([url]);
   }
+
   copyToClipboard(text: string): void {
-    navigator.clipboard.writeText(text).then(function() {
+    navigator.clipboard.writeText(text).then(function () {
       alert('Code copied to clipboard!');
     });
   }
 
-  inputText: string = '';
-  translatedText: string = '';
-
   translateText(): void {
+    if (this.translationMode === 'text-to-emoji') {
+      this.translateToEmoji();
+    } else if (this.translationMode === 'emoji-to-text') {
+      this.translateToText();
+    } else if (this.translationMode === 'add-emojis-to-text') {
+      this.addEmojisToText();
+    }
+  }
+
+  private translateToEmoji(): void {
     this.translatedText = this.inputText;
-    var EMOJIS = this.tool;
+    const EMOJIS = this.tool;
     for (const emojiKey in EMOJIS) {
       if (Object.prototype.hasOwnProperty.call(EMOJIS, emojiKey)) {
         const emoji = EMOJIS[emojiKey];
@@ -60,6 +81,35 @@ export class ToolComponent {
           this.translatedText = this.translatedText.replace(
             regex,
             emoji.char
+          );
+        });
+      }
+    }
+  }
+
+  private translateToText(): void {
+    this.translatedText = this.inputText;
+    const EMOJIS = this.tool;
+    for (const emojiKey in EMOJIS) {
+      if (Object.prototype.hasOwnProperty.call(EMOJIS, emojiKey)) {
+        const emoji = EMOJIS[emojiKey];
+        const regex = new RegExp(`${emoji.char}`, 'g');
+        this.translatedText = this.translatedText.replace(regex, emojiKey);
+      }
+    }
+  }
+
+  private addEmojisToText(): void {
+    this.translatedText = this.inputText;
+    const EMOJIS = this.tool;
+    for (const emojiKey in EMOJIS) {
+      if (Object.prototype.hasOwnProperty.call(EMOJIS, emojiKey)) {
+        const emoji = EMOJIS[emojiKey];
+        emoji.keywords.forEach((keyword: any) => {
+          const regex = new RegExp(`\\b${keyword}\\b`, 'gi');
+          this.translatedText = this.translatedText.replace(
+            regex,
+            `${keyword} ${emoji.char}`
           );
         });
       }
