b83d1c97b488067d71eaf8af64be78aba8fc4967
Jihad Sayed <david.pales1@gmail.com>
2025-03-31 10:27:22 -0500

    fix

--- src/app/shared/header/top-header/top-header.component.ts
+++ src/app/shared/header/top-header/top-header.component.ts
@@ -65,7 +65,7 @@ export class TopHeaderComponent {
     localStorage.removeItem('userInfo');
     this.router.navigate(['/pages/login']);
   }
-  goToAuth(action: 'signin' | 'signup'): void {
+  goToAuth(action: 'signIn' | 'signup'): void {
     const lang = this.languageService.getCurrentLanguage().toLowerCase();
   
     const langMap: { [key: string]: string } = {
