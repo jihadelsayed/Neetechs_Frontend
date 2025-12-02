37fe8bb19f8a8b8f7643720dbd96f11bec1f6120
Jihad Sayed <22003531+jihadelsayed@users.noreply.github.com>
2025-12-02 10:20:24 -0600

    Refactor templates to use Angular's @if and @else blocks
    Replaces most usages of *ngIf and <ng-template> with Angular's new @if and @else block syntax across multiple component templates. This improves readability and consistency, and prepares the codebase for future Angular updates. Minor related cleanups were also made, such as removing unused RouterLink import.

--- src/app/pages/homepage/home-how-it-works/home-how-it-works.component.ts
+++ src/app/pages/homepage/home-how-it-works/home-how-it-works.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-how-it-works',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './home-how-it-works.component.html',
   styleUrl: './home-how-it-works.component.scss'
 })
